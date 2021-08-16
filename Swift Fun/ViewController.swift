87024e4da193dd522fc832b0bf8668e82980893b
Te Awanui Reeder <teawanui@soulnoteagency.com>
2021-08-17 11:30:42 +1200

    Initial Commit

---
+++ Swift Fun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Fun
+//
+//  Created by Te Awanui Reeder on 17/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
