ea0603e5cbecb407e83abaa3770ce537f132d57b
Te Awanui Reeder <teawanui@soulnoteagency.com>
2021-08-21 11:13:11 +1200

    1st commit

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var buttonCount = 0
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBAction func buttonTapped(_ sender: Any)
+    
+    {buttonCount = buttonCount + 1
+        
+        print(buttonCount)
+        
+        if buttonCount >= 5 {
+            view.backgroundColor = UIColor.purple
+            
+        myLabel.text = "Mean ow, you hit me 5 times!"
+    }
+        
+        if buttonCount >= 10 {
+            view.backgroundColor = UIColor.blue
+            myLabel.text = "Mean ow, you hit me 10 times!"
+        }
+        
+        if buttonCount >= 15 {
+            view.backgroundColor = UIColor.yellow
+            myLabel.text = "Mean ow, you hit me 10 times!"
+            
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
     }
 
+ 
 
 }
 
